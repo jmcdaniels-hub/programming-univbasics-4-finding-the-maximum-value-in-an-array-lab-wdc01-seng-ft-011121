da78e978daeb8a224fa91b1d5fad47cf26478fe5
Jade McDaniels <jademcdaniels81@gmail.com>
2020-12-29 02:38:49 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,10 @@
-def find_max_value(array)
-  # Add your solution here
+def find_max_value(scale)
+  [2,3,4,5]
+  count = 0 
+  while count <= scale.length do
+    max_value = scale.max
+    index_value = max_value[count]
+    count +=1
+  end 
+ 
 end
