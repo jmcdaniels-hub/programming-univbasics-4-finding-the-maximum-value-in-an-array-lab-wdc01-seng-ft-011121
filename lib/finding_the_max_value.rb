f1f1fc237c7ec9d35e8e921c934860e8ee6891a2
Jade McDaniels <jademcdaniels81@gmail.com>
2020-12-29 02:40:51 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,7 +1,7 @@
 def find_max_value(scale)
   [2,3,4,5]
   count = 0 
-  while count <= scale.length do
+  while count < scale.length do
     max_value = scale.max
     index_value = max_value[count]
     count +=1
